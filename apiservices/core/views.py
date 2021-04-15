b676efc1ee764eca7b2b7567d3bf424fdea3d815
ombharatiya <om.bharatiya@verizonmedia.com>
2021-04-15 21:56:34 +0530

    Added Adventure 1

--- apiservices/core/views.py
+++ apiservices/core/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from rest_framework.response import Response
-from apiservices.core.constants import BLOG
+from apiservices.core.constants import BLOG, ADV_1
 
 
 @api_view(["GET"])
@@ -23,3 +23,13 @@ def blog(request):
     """
 
     return Response(BLOG, status=HTTP_200_OK)
+
+
+@api_view(["GET"])
+def adv_1(request):
+    """
+    :param request:
+    :return: JSON response
+    """
+
+    return Response(ADV_1, status=HTTP_200_OK)
